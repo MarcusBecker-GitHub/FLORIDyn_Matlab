8160376532807024671c7b76f938649d9b62fd35
Marcus Becker <marcus.becker@tudelft.nl>
2020-12-22 16:17:17 +0100

    typo

--- ObservationPoints/getChainIDforOP.m
+++
@@ -1,8 +0,0 @@
-function op_c = getChainIDforOP(chainList)
-% A for loop :(
-op_c = zeros(sum(chainList(:,3)),1);
-for i = 1:size(chainList,1)-1
-    op_c(chainList(i,2):chainList(i+1,2)-1) = i;
-end
-op_c(chainList(end,2):end)=size(chainList,1);
-end
